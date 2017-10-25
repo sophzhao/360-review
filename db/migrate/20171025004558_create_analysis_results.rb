2aaa75a994fa7589a9dc5248ad7bb70f240587b9
Sophie Zhao <syzhao@andrew.cmu.edu>
2017-10-24 20:55:43 -0400

    added analysis data

---
+++ db/migrate/20171025004558_create_analysis_results.rb
@@ -0,0 +1,11 @@
+class CreateAnalysisResults < ActiveRecord::Migration[5.1]
+  def change
+    create_table :analysis_results do |t|
+      t.integer :sentence_id
+      t.string :sentiment_label
+      t.float :sentiment_score
+
+      t.timestamps
+    end
+  end
+end
