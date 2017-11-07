3c12c1bc7eefe2a44da50dc203026a0d06a54d3e
Pooja Penninti <ppennint@andrew.cmu.edu>
2017-11-07 18:01:14 -0500

    card format implemented, reviews now shown

--- app/models/analysis_result.rb
+++ app/models/analysis_result.rb
@@ -1,5 +1,6 @@
 class AnalysisResult < ApplicationRecord
 
+ belongs_to :sentence
 
   scope :positive,       -> { where(sentiment_label: 'positive') }
   scope :negative,     	 -> { where(sentiment_label: 'negative') }
