1d89bf388403a577999931240fef11f910a4ea48
Sophie Zhao <syzhao@andrew.cmu.edu>
2017-11-07 13:08:53 -0500

    changes after meeting with prof h

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
 
     def index
-        AnalysisResult.run_api_analysis
+        # (sentence_id, sentiment_label, sentiment_score) = AnalysisResult.run_api_analysis
+        # @new_analysis_result = AnalysisResult.new
+        # @new_analysis_result.sentence_id = sentence_id
+        # @new_analysis_result.sentiment_label = sentiment_label
+        # @new_analysis_result.sentiment_score = sentiment_score
+
+        # @new_analysis_result
+        ar = AnalysisResult.new
+        ar.run_api_analysis(1)
+        @sentiment_score = ar.sentiment_score
+        # ar.save!
+
+
     end
     
 end
--- app/models/analysis_result.rb
+++ app/models/analysis_result.rb
@@ -1,4 +1,6 @@
 class AnalysisResult < ApplicationRecord
+    belongs_to :sentence
+
     # add api things 
     # create methods with requests
     # create new analysis results (add to db)
@@ -6,7 +8,7 @@ class AnalysisResult < ApplicationRecord
     require 'rest-client'
     require 'json'
 
-    def self.run_api_analysis
+    def run_api_analysis(sentence_id)
         # uri = URI.parse("https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=I%20still%20have%20a%20dream%2C%20a%20dream%20deeply%20rooted%20in%20the%20American%20dream%20%E2%80%93%20one%20day%20this%20nation%20will%20rise%20up%20and%20live%20up%20to%20its%20creed%2C%20%22We%20hold%20these%20truths%20to%20be%20self%20evident%3A%20that%20all%20men%20are%20created%20equal.&features=sentiment,keywords")
         # request = Net::HTTP::Get.new(uri)
         # request.basic_auth("5d047b97-caec-4304-a13b-ae28a9740c56", "WYmQLD06Ibra")
@@ -14,9 +16,17 @@ class AnalysisResult < ApplicationRecord
         response = RestClient::Request.execute method: :get, url: "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=I%20still%20have%20a%20dream%2C%20a%20dream%20deeply%20rooted%20in%20the%20American%20dream%20%E2%80%93%20one%20day%20this%20nation%20will%20rise%20up%20and%20live%20up%20to%20its%20creed%2C%20%22We%20hold%20these%20truths%20to%20be%20self%20evident%3A%20that%20all%20men%20are%20created%20equal.&features=sentiment,keywords", user: '5d047b97-caec-4304-a13b-ae28a9740c56', password: 'WYmQLD06Ibra'
 
         # render :json => response.body
-        puts response.body
         json_response = JSON.parse(response.body)
-        
+        self.sentence_id = sentence_id
+        sentiment_label = json_response["sentiment"]["document"]["label"]
+        sentiment_score = json_response["sentiment"]["document"]["score"]
+        puts sentiment_label
+        puts sentiment_score
+        self.sentiment_score = sentiment_score
+        self.sentiment_label = sentiment_label
+
+        # return (sentence_id, sentiment_label, sentiment_score)
+
     end
     
 end
